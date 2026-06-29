8399c60e394c8c60ee4c4c75e652f570773a344d
Shikhar Sinha <shikharsinha123abc@gmail.com>
2026-06-29 11:34:55 +0530

    Add: Sliding Window Maximum - Sliding Window

--- 04-Sliding-Window/06-sliding-window-maximum.java
+++ 04-Sliding-Window/06-sliding-window-maximum.java
@@ -1,8 +1,31 @@
 /*
 Problem: Sliding Window Maximum (LeetCode 239)
+Link: https://leetcode.com/problems/sliding-window-maximum/description/
 Pattern: Sliding Window
 Approach: Keep a deque (double ended queue) of indices that stays in decreasing order of value. Pop smaller values off the back when a bigger one comes in, and pop the front when it falls outside the window.
 Time: O(n) | Space: O(k)
 */
 
-// code goes here once solved
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] ans = new int[nums.length - k + 1];
+        int left = 0;
+        int x = 0;
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        for(int right = 0; right < nums.length; right++){
+            while(!dq.isEmpty() && dq.peekLast() < nums[right]){
+                dq.removeLast();
+            }
+            dq.addLast(nums[right]);
+
+            if(right - left + 1 == k){
+                ans[x++] = dq.peekFirst();
+                if(dq.peekFirst() == nums[left]){
+                    dq.removeFirst();
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+}
