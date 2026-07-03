35f23c0f00fc9c9cd97cc06bf5d9c0f544fd01dc
Shikhar Sinha <shikharsinha123abc@gmail.com>
2026-07-03 06:44:16 +0530

    Add: Contains Duplicate - Arrays and Hashing

--- 01-Arrays-and-Hashing/01-contains-duplicate.java
+++ 01-Arrays-and-Hashing/01-contains-duplicate.java
@@ -1,8 +1,19 @@
 /*
 Problem: Contains Duplicate (LeetCode 217)
+Link: https://leetcode.com/problems/contains-duplicate/
 Pattern: Arrays and Hashing
 Approach: Throw everything into a hashset while looping through the array. If a number is already in the set when you see it again, that's your duplicate.
 Time: O(n) | Space: O(n)
 */
 
-// code goes here once solved
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int val : nums){
+            if(!set.add(val)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
