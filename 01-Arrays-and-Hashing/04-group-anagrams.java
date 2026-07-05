c5d0eb0e779868daabf6bbd2d634dcde0241f3e3
Shikhar Sinha <shikharsinha123abc@gmail.com>
2026-07-05 09:46:34 +0530

    Add: Group Anagrams - Arrays and Hashing

--- 01-Arrays-and-Hashing/04-group-anagrams.java
+++ 01-Arrays-and-Hashing/04-group-anagrams.java
@@ -1,8 +1,23 @@
 /*
 Problem: Group Anagrams (LeetCode 49)
+Link: https://leetcode.com/problems/group-anagrams/
 Pattern: Arrays and Hashing
 Approach: For every string build a key (sorted version of the string works, or a count of each letter), then group all strings with the same key together in a hashmap.
 Time: O(n * k log k), k = avg string length | Space: O(n * k)
 */
 
-// code goes here once solved
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String str : strs){
+            int[] arr = new int[26];
+            for(char ch : str.toCharArray()){
+                arr[ch - 'a']++;
+            }
+            String key = Arrays.toString(arr);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
