bbd85622739acfaffcdeb52cc0472c772ef0160a
Shikhar Sinha <shikharsinha710@gmail.com>
2026-07-10 07:30:10 +0530

    Add: Two Sum II - Two pointers

--- 03-Two-Pointers/02-two-sum-ii.java
+++ 03-Two-Pointers/02-two-sum-ii.java
@@ -1,8 +1,24 @@
 /*
 Problem: Two Sum II (LeetCode 167)
+Link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
 Pattern: Two Pointers
 Approach: Since the array is already sorted, put one pointer at the start and one at the end. If the sum is too big move the right pointer left, if too small move the left pointer right.
 Time: O(n) | Space: O(1)
 */
 
-// code goes here once solved
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length-1;
+        while(right > left){
+            if(numbers[right] + numbers[left] == target){
+                return new int[]{left+1, right+1};
+            }else if(numbers[left] + numbers[right] < target){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return new int[2];
+    }
+}
