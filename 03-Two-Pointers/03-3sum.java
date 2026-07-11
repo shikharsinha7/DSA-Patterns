ce3cee6fdf9fe09c2d6f5e7a140779c21d3e123a
Shikhar Sinha <shikharsinha710@gmail.com>
2026-07-11 12:08:09 +0530

    Add: 3Sum - Two Pointers

--- 03-Two-Pointers/03-3sum.java
+++ 03-Two-Pointers/03-3sum.java
@@ -1,8 +1,39 @@
 /*
 Problem: 3Sum (LeetCode 15)
+Link: https://leetcode.com/problems/3sum/
 Pattern: Two Pointers
 Approach: Sort the array first. Fix one number, then use two pointers on the rest to find pairs that sum to the negative of the fixed number. Skip over duplicate values to avoid repeat triplets.
 Time: O(n^2) | Space: O(1) extra (not counting output)
 */
 
-// code goes here once solved
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i = 0; i < nums.length - 2; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    while(left < right && nums[left] == nums[left - 1]) left++;
+                    while(left < right && nums[right] == nums[right + 1]) right--;
+
+                } else if(sum < 0){
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+}
