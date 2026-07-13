07aae6c00fc69ca44e624c48a11ecf720b866e0b
Shikhar Sinha <shikharsinha710@gmail.com>
2026-07-13 13:02:19 +0530

    Add: Trapping Rain Water - Two Pointers

--- 03-Two-Pointers/05-trapping-rain-water.java
+++ 03-Two-Pointers/05-trapping-rain-water.java
@@ -1,8 +1,29 @@
 /*
 Problem: Trapping Rain Water (LeetCode 42)
+Link: https://leetcode.com/problems/trapping-rain-water/description/
 Pattern: Two Pointers
 Approach: Two pointers from both ends, tracking the max height seen so far from the left and from the right. Water trapped at any position is min(leftMax, rightMax) minus the height there.
 Time: O(n) | Space: O(1)
 */
 
-// code goes here once solved
+class Solution {
+    public int trap(int[] height) {
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+
+        leftMax[0] = height[0];
+        for(int i = 1; i < height.length; i++){
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+
+        rightMax[height.length-1] = height[height.length-1];
+        for(int i = height.length-2; i >= 0; i--){
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+        int water = 0;
+        for(int i = 0; i < height.length; i++){
+            water += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return water;
+    }
+}
