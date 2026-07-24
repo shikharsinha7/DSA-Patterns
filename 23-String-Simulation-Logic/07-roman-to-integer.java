3b8c09e5810de9d906d8f8fb06b2baf0ea371465
Shikhar Sinha <shikharsinha710@gmail.com>
2026-07-24 21:28:57 +0530

    Add: Roman to Integer - String Simulation Logic

--- 23-String-Simulation-Logic/07-roman-to-integer.java
+++ 23-String-Simulation-Logic/07-roman-to-integer.java
@@ -1,8 +1,30 @@
 /*
 Problem: Roman to Integer (LeetCode 13)
+Link: https://leetcode.com/problems/roman-to-integer/description/
 Pattern: String Simulation / In-place Logic
 Approach: Single left-to-right pass with a lookup map. If the current symbol's value is less than the next symbol's value, subtract it instead of adding (handles subtractive cases like IV, IX automatically without special-casing each one).
 Time: O(n) | Space: O(1)
 */
 
-// code goes here once solved
+class Solution {
+    public int romanToInt(String s) {
+        int val = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put('I' , 1);
+        map.put('V' , 5);
+        map.put('X' , 10);
+        map.put('L' , 50);
+        map.put('C' , 100);
+        map.put('D' , 500);
+        map.put('M' , 1000);
+
+        for(int i = 0; i < s.length(); i++){
+            if(i + 1 < s.length() && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))){
+                val -= map.get(s.charAt(i));
+            }else{
+                val += map.get(s.charAt(i));
+            }
+        }
+        return val;
+    }
+}
