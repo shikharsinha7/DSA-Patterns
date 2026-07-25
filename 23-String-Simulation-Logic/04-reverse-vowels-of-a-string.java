e8be7781f3fc85799eaa49ecbca9e1aaab4a2bbe
Shikhar Sinha <shikharsinha710@gmail.com>
2026-07-25 19:35:32 +0530

    Add: Reverse Vowels of a String - String Simulation Logic

--- 23-String-Simulation-Logic/04-reverse-vowels-of-a-string.java
+++ 23-String-Simulation-Logic/04-reverse-vowels-of-a-string.java
@@ -1,8 +1,37 @@
 /*
 Problem: Reverse Vowels of a String (LeetCode 345)
+Link: https://leetcode.com/problems/reverse-vowels-of-a-string/description/
 Pattern: String Simulation / In-place Logic
 Approach: Two pointers from both ends. Advance the left pointer until it lands on a vowel, advance the right pointer backward until it lands on a vowel, swap them, repeat until pointers cross. Consonants stay exactly where they are.
 Time: O(n) | Space: O(1)
 */
 
-// code goes here once solved
+class Solution {
+    public String reverseVowels(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        StringBuilder sb = new StringBuilder(s);
+        while(i < j){
+            while(i < j && !isVowel(sb.charAt(i))){
+                i++;
+            }
+            while(i < j && !isVowel(sb.charAt(j))){
+                j--;
+            }
+            if(i < j){
+                char temp = sb.charAt(i);
+                sb.setCharAt(i , sb.charAt(j));
+                sb.setCharAt(j , temp);
+                i++;
+                j--;
+            }
+        }
+        return sb.toString();
+    }
+    private boolean isVowel(char ch){
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+            return true;
+        }
+        return false;
+    }
+}
