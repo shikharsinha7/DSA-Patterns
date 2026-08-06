470db80bed5cc4150ba91b11907fc32c3ee66cc1
Shikhar Sinha <shikharsinha710@gmail.com>
2026-08-06 09:18:36 +0530

    Add: Set Matrix Zeroes - Math and Geometry

--- 20-Math-and-Geometry/03-set-matrix-zeroes.java
+++ 20-Math-and-Geometry/03-set-matrix-zeroes.java
@@ -1,8 +1,47 @@
 /*
 Problem: Set Matrix Zeroes (LeetCode 73)
+Link: https://leetcode.com/problems/set-matrix-zeroes/description/
 Pattern: Math and Geometry
 Approach: Use the first row and first column of the matrix itself as markers for which rows/columns need to become zero later, so you don't need extra space for tracking.
 Time: O(m*n) | Space: O(1)
 */
 
-// code goes here once solved
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int ROWS = matrix.length;
+        int COLS = matrix[0].length;
+        boolean rowZero = false;
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (matrix[r][c] == 0) {
+                    matrix[0][c] = 0;
+                    if (r > 0) {
+                        matrix[r][0] = 0;
+                    } else {
+                        rowZero = true;
+                    }
+                }
+            }
+        }
+
+        for (int r = 1; r < ROWS; r++) {
+            for (int c = 1; c < COLS; c++) {
+                if (matrix[0][c] == 0 || matrix[r][0] == 0) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int r = 0; r < ROWS; r++) {
+                matrix[r][0] = 0;
+            }
+        }
+        if (rowZero) {
+            for (int c = 0; c < COLS; c++) {
+                matrix[0][c] = 0;
+            }
+        }
+    }
+}
