04272cab4c86143994e9595c42dfce9d889c8d6c
Shikhar Sinha <shikharsinha710@gmail.com>
2026-08-09 10:38:53 +0530

    Add: Rotate Image - Math and Geometry

--- 20-Math-and-Geometry/01-rotate-image.java
+++ 20-Math-and-Geometry/01-rotate-image.java
@@ -1,8 +1,32 @@
 /*
 Problem: Rotate Image (LeetCode 48)
+Link: https://leetcode.com/problems/rotate-image/description/
 Pattern: Math and Geometry
 Approach: Rotate the matrix in place by first transposing it (flip rows and columns), then reversing every row — that combination gives you a 90 degree clockwise rotation.
 Time: O(n^2) | Space: O(1)
 */
 
-// code goes here once solved
+class Solution {
+    public void rotate(int[][] matrix) {
+        // transpose
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            int li = 0;
+            int ri = matrix[i].length - 1;
+            while (li < ri) {
+                int temp = matrix[i][li];
+                matrix[i][li] = matrix[i][ri];
+                matrix[i][ri] = temp;
+                li++;
+                ri--;
+            }
+        }
+    }
+}
