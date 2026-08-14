a8d06f7ce49702fa3c348291354614b71fdf9f4a
Shikhar Sinha <shikharsinha710@gmail.com>
2026-08-14 12:54:02 +0530

    Add: Move Zeroes - Array Simulation Logic

--- 22-Array-Simulation-Logic/02-move-zeroes.java
+++ 22-Array-Simulation-Logic/02-move-zeroes.java
@@ -1,8 +1,21 @@
 /*
 Problem: Move Zeroes (LeetCode 283)
+Link: https://leetcode.com/problems/move-zeroes/description/
 Pattern: Array Simulation / In-place Logic
 Approach: Maintain an "insert position" pointer. Walk through the array, and every time you see a non-zero element, swap it into the insert position and advance the pointer. By the end, all non-zero elements are shifted left in original relative order, and zeroes are naturally pushed to the back.
 Time: O(n) | Space: O(1)
 */
 
-// code goes here once solved
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZero = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[nonZero];
+                nums[nonZero] = temp;
+                nonZero++;
+            }
+        }
+    }
+}
