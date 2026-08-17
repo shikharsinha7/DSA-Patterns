d86724d9a174f97536aea5a41f10b4f384472cd8
Shikhar Sinha <shikharsinha710@gmail.com>
2026-08-17 23:07:43 +0530

    Add: Longest Happy Prefix - String Matching

--- 19-String-Matching/04-longest-happy-prefix.java
+++ 19-String-Matching/04-longest-happy-prefix.java
@@ -1,8 +1,36 @@
 /*
 Problem: Longest Happy Prefix (LeetCode 1392)
+Link: https://leetcode.com/problems/longest-happy-prefix/description/
 Pattern: String Matching (KMP / Rabin-Karp)
 Approach: This is exactly KMP's failure function — the longest prefix of the string that is also a suffix. The answer is just the last value in that prefix table.
 Time: O(n) | Space: O(n)
 */
 
-// code goes here once solved
+class Solution {
+    public String longestPrefix(String s) {
+        int[] LPS = computeLPS(s);
+        return s.substring(0, LPS[s.length() - 1]);
+    }
+    public int[] computeLPS(String s){
+        int m = s.length();
+        int[] LPS = new int[m];
+
+        int i = 1;
+        int len = 0;
+        while(i < m){
+            if(s.charAt(i) == s.charAt(len)){
+                len++;
+                LPS[i] = len;
+                i++;
+            }else{
+                if(len != 0){
+                    len = LPS[len - 1];
+                }else{
+                    LPS[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return LPS;
+    }
+}
