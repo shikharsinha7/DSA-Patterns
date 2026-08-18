7c4bd166394646c486631791571e56d5039bff6a
Shikhar Sinha <shikharsinha710@gmail.com>
2026-08-18 21:16:35 +0530

    Add: Invert Binary Tree - Trees BFS DFS

--- 10-Trees-BFS-DFS/01-invert-binary-tree.java
+++ 10-Trees-BFS-DFS/01-invert-binary-tree.java
@@ -1,8 +1,36 @@
 /*
 Problem: Invert Binary Tree (LeetCode 226)
+Link: https://leetcode.com/problems/invert-binary-tree/description/
 Pattern: Trees (BFS/DFS)
 Approach: Recursively swap the left and right children at every single node in the tree.
 Time: O(n) | Space: O(h), h = height of tree
 */
 
-// code goes here once solved
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
