4c9281a76823b724bbc68a306b46654c55cb04e9
Shikhar Sinha <shikharsinha710@gmail.com>
2026-08-20 18:08:44 +0530

    Add: Balanced Binary Tree - Trees BFS DFS

--- 10-Trees-BFS-DFS/04-balanced-binary-tree.java
+++ 10-Trees-BFS-DFS/04-balanced-binary-tree.java
@@ -1,8 +1,41 @@
 /*
 Problem: Balanced Binary Tree (LeetCode 110)
+Link: https://leetcode.com/problems/balanced-binary-tree/description/
 Pattern: Trees (BFS/DFS)
 Approach: DFS that returns the height of a subtree, but bail out early (return a sentinel value like -1) the moment any subtree is found to be unbalanced, instead of finishing the whole traversal.
 Time: O(n) | Space: O(h)
 */
 
-// code goes here once solved
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean flag = true;
+    public boolean isBalanced(TreeNode root) {
+        height(root);
+        return flag;
+    }
+    private int height(TreeNode root){
+        if(root == null) return 0;
+
+        int lheight = height(root.left);
+        int rheight = height(root.right);
+        if(Math.abs(lheight - rheight) > 1){
+            flag = false;
+        }
+        int theight = Math.max(lheight,rheight)+1;
+        return theight;
+    }
+}
