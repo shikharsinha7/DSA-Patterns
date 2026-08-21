6626af5ddf8f46a68caf8ad3cb0a86fb13d1cd50
Shikhar Sinha <shikharsinha710@gmail.com>
2026-08-21 22:21:31 +0530

    Add: Reverse Linked List - Linked List

--- 09-Linked-List/01-reverse-linked-list.java
+++ 09-Linked-List/01-reverse-linked-list.java
@@ -1,8 +1,33 @@
 /*
 Problem: Reverse Linked List (LeetCode 206)
+Link: https://leetcode.com/problems/reverse-linked-list/description/
 Pattern: Linked List
 Approach: Use three pointers — prev, curr, and next. Save curr's next node before you break the link, point curr back to prev, then shift everything one step forward.
 Time: O(n) | Space: O(1)
 */
 
-// code goes here once solved
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
