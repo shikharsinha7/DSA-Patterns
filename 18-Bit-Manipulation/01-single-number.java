fde675b53d97dd84dcad4d13d82ab03fbd827f07
Shikhar Sinha <shikharsinha710@gmail.com>
2026-08-25 22:54:28 +0530

    Add: Single Number - Bit Manipulation

--- 18-Bit-Manipulation/01-single-number.java
+++ 18-Bit-Manipulation/01-single-number.java
@@ -1,8 +1,17 @@
 /*
 Problem: Single Number (LeetCode 136)
+Link: https://leetcode.com/problems/single-number/description/
 Pattern: Bit Manipulation
 Approach: XOR every number in the array together. Every pair cancels itself out to 0, leaving only the one number that didn't have a pair.
 Time: O(n) | Space: O(1)
 */
 
-// code goes here once solved
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+}
