b459ec3c060c1c1ea3b9e8a2395e42a342f9a5f8
Shikhar Sinha <shikharsinha710@gmail.com>
2026-09-08 22:28:34 +0530

    Add: Kth Largest Element in an Array - Sorting Implementation

--- 21-Sorting-Implementation/02-kth-largest-element-in-an-array.java
+++ 21-Sorting-Implementation/02-kth-largest-element-in-an-array.java
@@ -1,8 +1,50 @@
 /*
 Problem: Kth Largest Element in an Array (LeetCode 215)
+Link: https://leetcode.com/problems/kth-largest-element-in-an-array/description/
 Pattern: Sorting (Implementation Level)
 Approach: Quickselect — basically quicksort's partitioning step, but you only ever recurse into the side of the partition that actually contains the kth element, skipping the other side entirely.
 Time: O(n) average, O(n^2) worst case | Space: O(1) in-place
 */
 
-// code goes here once solved
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        k = nums.length - k;
+        return quickSelect(nums, 0, nums.length - 1, k);
+    }
+    private int quickSelect(int[] nums, int l, int r, int k){
+        
+        int pivotIndex = l + (int)(Math.random() * (r - l + 1));
+        int pivot = nums[pivotIndex];
+
+        int low = l;
+        int i = l;
+        int high = r;
+
+        while(i <= high){
+            if(nums[i] < pivot){
+                int temp = nums[low];
+                nums[low] = nums[i];
+                nums[i] = temp;
+                low++;
+                i++;
+            }else if(nums[i] > pivot){
+                int temp = nums[i];
+                nums[i] = nums[high];
+                nums[high] = temp;
+                high--;
+            }else{
+                i++;
+            }
+        }
+        
+        if(k < low){
+            return quickSelect(nums, l, low - 1, k);
+        }
+        else if(k > high){
+            return quickSelect(nums, high + 1, r, k);
+        }
+        else{
+            return nums[k];
+        }
+    }
+}
