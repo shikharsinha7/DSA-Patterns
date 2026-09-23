3a4de958ce9fa5a75e20012e2e674b0e54492ffd
Shikhar Sinha <shikharsinha710@gmail.com>
2026-09-23 23:44:39 +0530

    Add: Number of 1 Bits - Bit Manipulation

--- 18-Bit-Manipulation/02-number-of-1-bits.java
+++ 18-Bit-Manipulation/02-number-of-1-bits.java
@@ -1,8 +1,18 @@
 /*
 Problem: Number of 1 Bits (LeetCode 191)
+Link: https://leetcode.com/problems/number-of-1-bits/description/
 Pattern: Bit Manipulation
 Approach: Repeatedly check the last bit using n & 1 and right shift, or use the slightly faster trick n & (n-1) which drops the lowest set bit each time.
 Time: O(32) i.e. O(1) for fixed bit width | Space: O(1)
 */
 
-// code goes here once solved
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+}
