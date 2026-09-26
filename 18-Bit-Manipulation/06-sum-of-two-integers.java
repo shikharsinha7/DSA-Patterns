f522b23bcce954bbaee53501a3a1a0773b1adeab
Shikhar Sinha <shikharsinha710@gmail.com>
2026-09-26 22:17:03 +0530

    Add: Sum of Two Integers - Bit Manipulation

--- 18-Bit-Manipulation/06-sum-of-two-integers.java
+++ 18-Bit-Manipulation/06-sum-of-two-integers.java
@@ -1,8 +1,18 @@
 /*
 Problem: Sum of Two Integers (LeetCode 371)
+Link: https://leetcode.com/problems/sum-of-two-integers/description/
 Pattern: Bit Manipulation
 Approach: Simulate addition manually using XOR for the sum-without-carry part and AND+shift for the carry part, repeating until there's no carry left.
 Time: O(32) i.e. O(1) | Space: O(1)
 */
 
-// code goes here once solved
+class Solution {
+    public int getSum(int a, int b) {
+        while(b != 0){
+            int tmp = (a & b) << 1;
+            a = a ^ b;
+            b = tmp;
+        }
+        return a;
+    }
+}
